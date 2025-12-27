dc0f13062b69ecc28e0a03ccb5db4bfb780fc01b
steve B <stephenbrousseau89@gmail.com>
2025-12-26 19:41:47 -0700

    fall back to browserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react'
-import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 import Home from './components/Home'
